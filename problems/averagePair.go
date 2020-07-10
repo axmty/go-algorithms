e6187fcee7cc8e5dfc8b3d6a5e9723428e513d2c
axmty <alexandre.metgy@gmail.com>
2020-07-11 01:07:20 +0200

    Implement AveragePair.

--- problems/averagePair.go
+++ problems/averagePair.go
@@ -3,5 +3,18 @@ package problems
 // AveragePair determines if there is a pair of values in arr where the average of the pair equals
 // the target average.
 func AveragePair(arr []int, target float32) bool {
-	return false
+	sorted := make([]int, len(arr))
+	copy(sorted, arr)
+	begin, end := 0, len(sorted)-1
+	for begin < end {
+		avg := (float32(sorted[begin]) + float32(sorted[end])) / 2
+		if avg == target {
+			break
+		} else if avg < target {
+			begin++
+		} else {
+			end--
+		}
+	}
+	return begin < end
 }
