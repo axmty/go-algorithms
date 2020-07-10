24376be18e94419ccb229107274e15282e4a7f63
axmty <alexandre.metgy@gmail.com>
2020-07-10 16:32:01 +0200

    Implement ValidAnagram.

--- problems/validAnagram.go
+++ problems/validAnagram.go
@@ -2,5 +2,22 @@ package problems
 
 // ValidAnagram determines if string b is an anagram of string a.
 func ValidAnagram(a, b string) bool {
-	return false
+	if len(a) != len(b) {
+		return false
+	}
+	lookup := make(map[rune]int)
+	for _, r := range a {
+		if _, ok := lookup[r]; !ok {
+			lookup[r] = 1
+		} else {
+			lookup[r]++
+		}
+	}
+	for _, r := range b {
+		if n, ok := lookup[r]; !ok || n == 0 {
+			return false
+		}
+		lookup[r]--
+	}
+	return true
 }
