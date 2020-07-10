a68af62c40c3bde6e15549267e84bcdb95f9f1b0
axmty <alexandre.metgy@gmail.com>
2020-07-11 00:33:44 +0200

    Write test for AveragePair.

---
+++ problems/averagePair.go
@@ -0,0 +1,7 @@
+package problems
+
+// AveragePair determines if there is a pair of values in arr where the average of the pair equals
+// the target average.
+func AveragePair(arr []int, target float32) bool {
+	return false
+}
