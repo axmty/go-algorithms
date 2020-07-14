6159dec88101a1069437a692de2c0ad9ad578aff
axmty <alexandre.metgy@gmail.com>
2020-07-14 12:54:48 +0200

    Add some cases to Bubble sort test and use common cases to test sort algorithms.

--- sorts/sorts_test.go
+++ sorts/sorts_test.go
@@ -5,14 +5,18 @@ import (
 	"testing"
 )
 
+var sortTestCases = []struct {
+	arr  []int
+	want []int
+}{
+	{[]int{}, []int{}},
+	{[]int{2}, []int{2}},
+	{[]int{2, 1}, []int{1, 2}},
+	{[]int{6, 5, 3, 1, 5, 8, 7, 7, 2, 4}, []int{1, 2, 3, 4, 5, 5, 6, 7, 7, 8}},
+}
+
 func TestBubble(t *testing.T) {
-	cases := []struct {
-		arr  []int
-		want []int
-	}{
-		{[]int{6, 5, 3, 1, 5, 8, 7, 7, 2, 4}, []int{1, 2, 3, 4, 5, 5, 6, 7, 7, 8}},
-	}
-	for _, c := range cases {
+	for _, c := range sortTestCases {
 		arg := append([]int(nil), c.arr...)
 		Bubble(c.arr)
 		if !reflect.DeepEqual(c.arr, c.want) {
