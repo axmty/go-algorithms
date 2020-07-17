3dc5f555ee2c8a00dcd0ffac2ed7383048c6f0d8
axmty <alexandre.metgy@gmail.com>
2020-07-18 00:34:04 +0200

    Implement singlelist Back, Len and fix InsertAfter.

--- structs/singlelist/singlelist.go
+++ structs/singlelist/singlelist.go
@@ -2,14 +2,13 @@ package singlelist
 
 // Elem is an element of a linked list.
 type Elem struct {
-	next  *Elem
 	Value interface{}
+	next  *Elem
+	list  *List
 }
 
 // Next returns the next list element or nil.
-func (e *Elem) Next() *Elem {
-	return e.next
-}
+func (e *Elem) Next() *Elem { return e.next }
 
 // List represents a singly linked list data structure.
 type List struct {
@@ -34,14 +33,31 @@ func (l *List) Init() *List {
 }
 
 // Front returns the first element of list l or nil if the list is empty.
-func (l *List) Front() *Elem {
-	return l.front
+func (l *List) Front() *Elem { return l.front }
+
+// Back returns the last element of list l or nil if the list is empty.
+func (l *List) Back() *Elem {
+	if l.len == 0 {
+		return nil
+	}
+	e := l.front
+	for e.next != nil {
+		e = e.next
+	}
+	return e
 }
 
 // InsertAfter inserts a new element e with value v immediately after mark and returns e.
 // If mark is not an element of l, the list is not modified. The mark must not be nil.
 func (l *List) InsertAfter(v interface{}, mark *Elem) *Elem {
-	e := &Elem{Value: v, next: mark.next}
+	if mark.list != l {
+		return nil
+	}
+	e := &Elem{Value: v, next: mark.next, list: l}
 	mark.next = e
+	l.len++
 	return e
 }
+
+// Len returns the number of elements of list l. The complexity is O(1).
+func (l *List) Len() int { return l.len }
